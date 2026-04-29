a96aff4476f86bc51c63a27c9450f56a72ce5c98
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-04-29 20:33:14 +0530

    frist lecture is done.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function greet(person: string): string{
+    return `Hello ${person}, welcome to vectosmind`;
+}
+
+const username: string = "chai aur Typescript"
+
+console.log(greet(username))
---
+++ src/typesInTs.ts
@@ -0,0 +1,26 @@
+// Type infarancing
+let drink = "chai"
+
+let cup = Math.random() > 0.5 ? 10 : '5'
+
+/*
+## Error
+    Two type of error: 1.Syntex error, 2.Type error 
+
+
+// SyntaxError: let let drink = "chai"
+
+// Type error:
+let channelName = "ChaiaurCode"
+channelName = 12123
+ */
+
+let channelName = "ChaiaurCode"
+// channelName = 12123
+let chaiFlavor = "Ginger tea"
+chaiFlavor = "gorom tam"
+
+let isPublic: boolean = true
+
+// Type enotation: User push to try understand
+// Type infarance: Auto understand
